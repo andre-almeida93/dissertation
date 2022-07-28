9500e9f905e1542875d55c7878b50657a6a318c0
unknown <ande-r-almeida@hotmail.com>
2022-07-28 19:12:10 +0100

    Updates

---
+++ precache-manifest.f654325ee6c7c56aeb22efcddd6cdcc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd63848b0456a8d077d5",
+    "url": "/dissertation/static/css/main.2b0bfd69.chunk.css"
+  },
+  {
+    "revision": "bd63848b0456a8d077d5",
+    "url": "/dissertation/static/js/main.bd63848b.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/dissertation/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/dissertation/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "bbbd928c50de4814e9f4",
+    "url": "/dissertation/static/js/runtime~main.bbbd928c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/dissertation/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c958963515fbd352ea159e3af996f002",
+    "url": "/dissertation/index.html"
+  }
+];
